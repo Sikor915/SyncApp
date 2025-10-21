d05557ecf65b1b6206b2007a48aca331b646e4e0
Kacper Sikorski <115071106+Sikor915@users.noreply.github.com>
2025-10-21 13:55:26 +0200

    Starter commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SyncApp
+{
+    class SyncApp
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage: SyncApp <sourceDirectory> <targetDirectory> <logDirectory>");
+                return;
+            }
+            Console.WriteLine("Welcome to SyncApp!");
+            // Application logic goes here
+        }
+    }
+}
