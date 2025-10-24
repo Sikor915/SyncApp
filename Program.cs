559136163e76463148c27da56e34e7672ce0d542
Kacper Sikorski <115071106+Sikor915@users.noreply.github.com>
2025-10-24 20:13:47 +0200

    Removed unnecessary code

--- Program.cs
+++ Program.cs
@@ -36,31 +36,21 @@ namespace SyncApp
             }
 
             Console.WriteLine("Welcome to SyncApp!");
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                WindowsSync();
-                SetUpTimer(intervalInMinutes, true);
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                LinuxSync();
-                SetUpTimer(intervalInMinutes, false);
-            }
+            
+            Sync();
+            SetUpTimer(intervalInMinutes);
 
             Console.WriteLine("The application will run in the background. Press Enter to exit.");
             Console.ReadLine();
         }
 
-        static void WindowsSync()
+        static void Sync()
         {
-            Console.WriteLine("Syncing on Windows...");
-
             if (!Path.Exists(log))
             {
                 Directory.CreateDirectory(log);
             }
-            
+
             string logFilePath = Path.Combine(log, "sync_" + DateTime.Now.ToString("yyyyMMdd") + ".log");
             File.AppendAllText(logFilePath, "-----------------\nSync started at " + DateTime.Now + "\n");
 
@@ -71,59 +61,13 @@ namespace SyncApp
 
                 return;
             }
-            
+
             DirectoryInfo sourceDir = new(source);
             DirectoryInfo targetDir = new(target);
 
             foreach (var file in sourceDir.GetFiles("*", SearchOption.AllDirectories))
             {
-                Console.WriteLine("Processing source file: {0}", file.FullName);
-                File.AppendAllText(logFilePath, "Processing source file: {file.FullName}\n");
-
-                string relativePath = Path.GetRelativePath(source, file.FullName);
-                sourceFiles[relativePath] = new FileStuff(relativePath, file);
-            }
-
-            foreach (var file in targetDir.GetFiles("*", SearchOption.AllDirectories))
-            {
-                Console.WriteLine("Processing target file: {0}", file.FullName);
-                File.AppendAllText(logFilePath, $"Processing target file: {file.FullName}\n");
-
-                string relativePath = Path.GetRelativePath(target, file.FullName);
-                targetFiles[relativePath] = new FileStuff(relativePath, file);
-            }
-
-            RemoveEntries(logFilePath);
-
-            UpdateEntries(logFilePath);
-        }
-
-        static void LinuxSync()
-        {
-            Console.WriteLine("Syncing on Linux...");
-
-            if (!Path.Exists(log))
-            {
-                Directory.CreateDirectory(log);
-            }
-            
-            string logFilePath = Path.Combine(log, "sync_" + DateTime.Now.ToString("yyyyMMdd") + ".log");
-            File.AppendAllText(logFilePath, "-----------------\nSync started at " + DateTime.Now + "\n");
-
-            if (!Path.Exists(source) || !Path.Exists(target))
-            {
-                Console.WriteLine("One or more specified directories do not exist.");
-                File.AppendAllText(logFilePath, "One or more specified directories do not exist.\n");
-
-                return;
-            }
-            
-            DirectoryInfo sourceDir = new(source);
-            DirectoryInfo targetDir = new(target);
-
-            foreach (var file in sourceDir.GetFiles("*", SearchOption.AllDirectories))
-            {
-                Console.WriteLine("Processing source file: {0}", file.FullName);
+                Console.WriteLine($"Processing source file: {file.FullName}");
                 File.AppendAllText(logFilePath, $"Processing source file: {file.FullName}\n");
 
                 string relativePath = Path.GetRelativePath(source, file.FullName);
@@ -132,7 +76,7 @@ namespace SyncApp
 
             foreach (var file in targetDir.GetFiles("*", SearchOption.AllDirectories))
             {
-                Console.WriteLine("Processing target file: {0}", file.FullName);
+                Console.WriteLine($"Processing target file: {file.FullName}");
                 File.AppendAllText(logFilePath, $"Processing target file: {file.FullName}\n");
 
                 string relativePath = Path.GetRelativePath(target, file.FullName);
@@ -143,14 +87,14 @@ namespace SyncApp
 
             UpdateEntries(logFilePath);
         }
-
+        
         static void RemoveEntries(string logPath)
         {
             foreach (var targetEntry in targetFiles)
             {
                 if (!sourceFiles.ContainsKey(targetEntry.Key))
                 {
-                    Console.WriteLine("Deleting file: {0}", targetEntry.Key);
+                    Console.WriteLine($"Deleting file: {targetEntry.Key}");
                     File.AppendAllText(logPath, $"Deleting file: {targetEntry.Key}\n");
 
                     string targetFilePath = Path.Combine(target, targetEntry.Key);
@@ -168,7 +112,7 @@ namespace SyncApp
                     if (sourceEntry.Value.File.Length != targetFileStuff.File.Length ||
                         sourceEntry.Value.File.LastWriteTimeUtc != targetFileStuff.File.LastWriteTimeUtc)
                     {
-                        Console.WriteLine("Updating file: {0}", sourceEntry.Key);
+                        Console.WriteLine($"Updating file: {sourceEntry.Key}");
                         File.AppendAllText(logPath, $"Updating file: {sourceEntry.Key}\n");
 
                         string targetFilePath = Path.Combine(target, sourceEntry.Key);
@@ -178,7 +122,7 @@ namespace SyncApp
                 }
                 else
                 {
-                    Console.WriteLine("Copying new file: {0}", sourceEntry.Key);
+                    Console.WriteLine($"Copying new file: {sourceEntry.Key}");
                     File.AppendAllText(logPath, $"Copying new file: {sourceEntry.Key}\n");
 
                     string targetFilePath = Path.Combine(target, sourceEntry.Key);
@@ -188,26 +132,18 @@ namespace SyncApp
             }
         }
 
-        static void SetUpTimer(int intervalInMinutes, bool isWindows)
+        static void SetUpTimer(int intervalInMinutes)
         {
             intervalTimer = new Timer(intervalInMinutes * 60 * 1000);
-            intervalTimer.Elapsed += (sender, e) => OnIntervalEvent(isWindows);
+            intervalTimer.Elapsed += (sender, e) => OnIntervalEvent();
             intervalTimer.AutoReset = true;
             intervalTimer.Enabled = true;
         }
 
-        static void OnIntervalEvent(bool isWindows)
+        static void OnIntervalEvent()
         {
             Console.WriteLine("Interval elapsed, performing sync...");
-
-            if (isWindows)
-            {
-                WindowsSync();
-            }
-            else
-            {
-                LinuxSync();
-            }
+            Sync();
         }
     }
     class FileStuff
